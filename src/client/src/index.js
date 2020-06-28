0bb7d4ea9b232136932717eb0cc39194e581f818
hanhhn <hngochanh@outlook.comm>
2020-06-28 12:18:45 +0700

    import antd/dist/antd.css

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './app';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import "antd/dist/antd.css";
+import App from "./app";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
